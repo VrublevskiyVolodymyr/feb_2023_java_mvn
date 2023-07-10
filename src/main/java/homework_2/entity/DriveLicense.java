0377a46caaa0d6829f508b49a604fa32c65d9ade
VrublevskiyVolodymyr <115405839+VrublevskiyVolodymyr@users.noreply.github.com>
2023-07-10 12:16:07 +0300

    change

--- src/main/java/homework_2/entity/DriveLicense.java
+++ src/main/java/homework_2/entity/DriveLicense.java
@@ -1,11 +1,17 @@
 package homework_2.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString(exclude = "owner")
+
 
 public class DriveLicense {
     @Id
@@ -13,6 +19,8 @@ public class DriveLicense {
     private int id;
     private String series;
 
+    @OneToOne(cascade = {CascadeType.ALL}, mappedBy = "driveLicense", fetch = FetchType.LAZY)
+    private Owner owner;
 
     public DriveLicense(String series) {
         this.series = series;
